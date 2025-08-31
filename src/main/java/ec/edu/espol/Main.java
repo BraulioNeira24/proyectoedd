aeaa51e48be91ce63bf171aae456fc30d6448e0c
Hack-Jonathan <jpacalla@espol.edu.ec>
2025-08-31 14:57:18 -0500

    JavaFX Parte 3 - Creacion de Aeropuertos

--- src/main/java/ec/edu/espol/Main.java
+++ src/main/java/ec/edu/espol/Main.java
@@ -1,5 +1,7 @@
 package ec.edu.espol;
 
+import java.util.Comparator;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
@@ -7,16 +9,15 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 // import javafx.scene.control.Alert;
 // import javafx.scene.control.Button;
 // import javafx.scene.control.ButtonType;
 // import javafx.scene.control.TextField;
 // import javafx.scene.control.Label;
-// 
-
-// import javafx.scene.paint.Color;
 // import javafx.scene.input.MouseEvent;
-
+// import java.io.*;
+// import java.util.*;
 
 public class Main extends Application {
     private final String ARCHIVO_DATOS = "datosAeropuertos.dat";
@@ -45,11 +46,38 @@ public class Main extends Application {
         stage.setScene(scene); //Muestra la ventana principal.
         stage.show();
 
+        //Comparator para comprar codigos de aeropuerto 
+        Comparator<String> cmp = (a,b) -> a.compareToIgnoreCase(b);
+        Red red = new Red(cmp);
+
+        //Agrega algunos aeropuertos de ejemplo
+        red.addAeropuerto(new Aeropuerto("GYE","Guayaquil", "Guayaquil", "Ecuador", -2.157, -79.883));
+        red.addAeropuerto(new Aeropuerto("UIO","Quito", "Quito", "Ecuador", -0.141, -78.488));
+        red.addAeropuerto(new Aeropuerto("CUE","Cuenca", "Cuenca", "Ecuador", -2.900, -78.983));
+        red.addAeropuerto(new Aeropuerto("MIA","Miami", "Miami", "EEUU", 25.7617, -80.1918));
+        red.addAeropuerto(new Aeropuerto("LIM","Lima", "Lima", "Perú", -12.0464, -77.0428));
+        red.addAeropuerto(new Aeropuerto("MAD","Madrid", "Madrid", "España", 40.4168, -3.7038));
+        red.addAeropuerto(new Aeropuerto("CDG","París", "París", "Francia", 49.0097, 2.5479));
+        red.addAeropuerto(new Aeropuerto("NRT","Tokio", "Tokio", "Japón", 35.7767, 140.3186));
+        red.addAeropuerto(new Aeropuerto("SYD","Sídney", "Sídney", "Australia", -33.8688, 151.2093));
+        red.addAeropuerto(new Aeropuerto("AKL","Auckland", "Auckland", "Nueva Zelanda", -37.0083, 174.7947));
     
+        for(Aeropuerto a : red.getVertices()){
+            double x = lonToX(a.getLongitud(), canvasWidth);
+            double y = latToY(a.getLatitud(), canvasHeight);
+            gc.setFill(Color.RED); // Color del aeropuerto
+            gc.fillOval(x - 5, y - 5, 10, 10); // Dibuja un circulo de radio 5
+            gc.setFill(Color.BLACK);
+            gc.fillText(a.getCodigo(), x + 8, y); // Escribe el nombre al lado del aeropuerto
+        }
     }
 
-
-
+    private double lonToX(double lon,double canvasWidth){
+        return ((lon+180)/360.0)*canvasWidth;  // Convierte longitud a coordenadas en X
+    }
+    private double latToY(double lat,double canvasHeight){
+        return ((90 -lat)/180.0)* canvasHeight; // Convierte latitud a coordenadas en Y
+    }
 
     public static void main(String[] args) {
         launch(args);
