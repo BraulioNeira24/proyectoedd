1e169fe239c3585310f93d2eb25f29b30311a50e
Hack-Jonathan <jpacalla@espol.edu.ec>
2025-08-31 19:49:22 -0500

    Nueva Actualizacion

--- src/main/java/ec/edu/espol/Main.java
+++ src/main/java/ec/edu/espol/Main.java
@@ -24,16 +24,10 @@ public class Main extends Application {
     private Random randomColor = new Random();
     private Map<Aeropuerto, Color> coloresAeropuertos = new HashMap<>();
 
-
-
-
-
     @Override
     public void start(Stage stage) {
         //Crear el Pane principal
         Pane root = new Pane(); //Crea un contenedor para los elementos gráficos.
-
-        
 
         // Cargar la imagen del mapa 
         Image mapa = new Image(getClass().getResourceAsStream("/mapa.png"));
@@ -43,13 +37,20 @@ public class Main extends Application {
         // Crear el Canvas (por ejemplo, de 900x600 pixeles)
         Canvas canvas = new Canvas(canvasWidth, canvasHeight); //Crea el área de dibujo donde se mostrará el mapa y los elementos.
         root.getChildren().add(canvas); //Añade el canvas al contenedor.
-        // Boton agregar aeropuerto 
+        /*  
+         * Boton agregar aeropuerto
+        */
         Button btnAgregarAeropuerto = new Button("Agregar Aeropuerto");
         btnAgregarAeropuerto.setLayoutX(20); //Posiciona horizontal en la ventana
         btnAgregarAeropuerto.setLayoutY(20); //Posicionavertical en la ventana
         root.getChildren().add(btnAgregarAeropuerto); // Añadir el botón a la ventana
-
-        
+        /*
+         * Boton Reiniciar aeropuertos
+        */
+        Button btnReiniciar = new Button("Reiniciar Aeropuertos");
+        btnReiniciar.setLayoutX(20); //Posiciona horizontal en la ventana
+        btnReiniciar.setLayoutY(60); //Posiciona vertical en la ventana
+        root.getChildren().add(btnReiniciar); // Añadir el botón a la ventana
         //Dibujar la imagen en el canvas (sin escalado)
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.drawImage(mapa, 0, 0, canvasWidth, canvasHeight);
@@ -61,73 +62,15 @@ public class Main extends Application {
         stage.show();
 
         /*
-         * AEROPUERTOS
+         * CREACION MAPA
          */
         //Comparator para comprar codigos de aeropuerto 
         Comparator<String> cmp = (a,b) -> a.compareToIgnoreCase(b);
         Red red = new Red(cmp);
-
-        //Agrega algunos aeropuertos de ejemplo
-        red.addAeropuerto(new Aeropuerto("PKX", "Daxing","Pekín","China",39.511944, 116.410556));
-        red.addAeropuerto(new Aeropuerto("UIO","Quito", "Quito", "Ecuador", -0.141, -78.488));
-        red.addAeropuerto(new Aeropuerto("MIA","Miami", "Miami", "EEUU", 25.7617, -80.1918));
-        red.addAeropuerto(new Aeropuerto("MAD","Madrid", "Madrid", "España", 40.4168, -3.7038));
-        red.addAeropuerto(new Aeropuerto("CDG","París", "París", "Francia", 49.0097, 2.5479));
-        red.addAeropuerto(new Aeropuerto("NRT","Tokio", "Tokio", "Japón", 35.7767, 140.3186));
-        red.addAeropuerto(new Aeropuerto("SYD","Sídney", "Sídney", "Australia", -33.8688, 151.2093));
-        red.addAeropuerto(new Aeropuerto("AKL","Auckland", "Auckland", "Nueva Zelanda", -37.0083, 174.7947));
-        red.addAeropuerto(new Aeropuerto("SN", "Santo Domingo", "Santo Domingo", "República Dominicana", 18.4861, -69.9312));
-        red.addAeropuerto(new Aeropuerto("DXB", "Dubai International", "Dubái", "Emiratos Árabes Unidos", 25.2532, 55.3657));
-
+        cargarDatosIniciales(red);
+        coloresAeropuertos.clear();
+        redibujarMapa(gc, mapa, canvasWidth, canvasHeight, red, coloresAeropuertos);
         
-        for(Aeropuerto a : red.getVertices()){
-            // Asigna un color aleatorio si no tiene uno asignado
-            Color colorAl = generarColorAleatorio();
-            coloresAeropuertos.put(a,colorAl);
-            double x = lonToX(a.getLongitud(), canvasWidth);
-            double y = latToY(a.getLatitud(), canvasHeight);
-            gc.setFill(colorAl); // Color del aeropuerto
-            gc.fillOval(x - 5, y - 5, 10, 10); // Dibuja un circulo de radio 5
-            gc.setFill(Color.BLACK);
-            gc.fillText(a.getCodigo(), x + 8, y); // Escribe el nombre al lado del aeropuerto
-        }
-
-        /*
-         * VUELOS
-         */
-        red.addVuelo("PKX", "DXB", 220, 200.15,"China Airlines");
-        red.addVuelo("PKX", "CDG", 340, 230.23,"China Airlines");
-        red.addVuelo("UIO", "MIA", 230, 600.30,"LATAM Airlines");
-        red.addVuelo("NRT", "MAD" , 542, 438,"Avianca");
-        red.addVuelo("UIO", "LIM", 70, 123.12,"LATAM Airlines");
-        red.addVuelo("LIM", "MIA", 70, 230.30,"LATAM Airlines");
-        red.addVuelo("MIA", "NRT", 340, 600.29,"American Airlines");
-        red.addVuelo("SYD", "NRT", 320, 502.53,"Avianca");
-        red.addVuelo("SN", "MAD", 130, 134.32,"Avianca");
-
-        //Recorre los vuelos y dibuja las rutas
-        for(Aeropuerto origen : red.getVertices()){ // Recorre los aeropuertos de la red
-            for(Vuelos vuelo : origen.getAdyacentes()){ // Recorre los vuelos adyacentes de cada aeropuerto
-                Aeropuerto destino = vuelo.getTarget(); // Obtiene el Aeropuerto de destino de ese vuelo
-                // Calculan la posición (X, Y) del aeropuerto de origen en el canvas.
-                double x1 = lonToX(origen.getLongitud(), canvasWidth);
-                double y1 = latToY(origen.getLatitud(), canvasHeight);
-                // Calculan la posición (X, Y) del aeropuerto de destino en el canvas.
-                double x2 = lonToX(destino.getLongitud(), canvasWidth);
-                double y2 = latToY(destino.getLatitud(), canvasHeight);
-                // Dibuja la linea del vuelo 
-                gc.setStroke(coloresAeropuertos.get(origen)); // Color de la línea del vuelo origen
-                gc.setLineWidth(2); // Grosor linea
-                gc.strokeLine(x1, y1, x2, y2); // Dibuja la linea que representa el vuelo
-                // Dibuja la flecha en el extremo de la línea
-                drawArrow(gc, x1, y1, x2, y2);
-                //Calculo el punto medio 
-                double midX = (x1 + x2) / 2;
-                double midY = (y1 + y2) / 2;
-                gc.setFill(Color.BLACK); //Color texto
-                gc.fillText(vuelo.getDistancia() + " km", midX, midY - 5); // Escribe la distancia en el punto medio
-            }
-        }
         /*
          * ZOOM
         */
@@ -220,15 +163,12 @@ public class Main extends Application {
             }
         });        
 
-
-
-
-
-
-
-
-
-
+        // Reiniciar aeropuertos
+        btnReiniciar.setOnAction(e ->{
+            coloresAeropuertos.clear();
+            cargarDatosIniciales(red);
+            redibujarMapa(gc, mapa, canvasWidth, canvasHeight, red, coloresAeropuertos);
+        });
 
 
     }
@@ -246,7 +186,6 @@ public class Main extends Application {
         return Color.color(randomColor.nextDouble(), randomColor.nextDouble(), randomColor.nextDouble());
     }
 
-    
     // Dibuja una flecha entre dos puntos (Gc es el que me permite dibujar en canvas)
     private void drawArrow(GraphicsContext gc, double x1, double y1, double x2, double y2){
         double angle = Math.atan2(y2 - y1, x2 - x1); // Calcula el ángulo de la línea entre el punto de origen (x1, y1) y el destino (x2, y2)
@@ -267,7 +206,78 @@ public class Main extends Application {
         gc.strokeLine(x2,y2,xArrow2,yArrow2);
     }
     
+    //Metodo para redibujar el mapa compelto (Aeropuerto y vuelos)
+    private void redibujarMapa(GraphicsContext gc, Image mapa, double canvasWidth,double canvasHeight, Red red, Map<Aeropuerto, Color> coloresAeropuerto){
+        gc.clearRect(0, 0,canvasWidth, canvasHeight);
+        gc.drawImage(mapa, 0, 0, canvasWidth, canvasHeight);
+        for(Aeropuerto a : red.getVertices()){
+            // Asigna un color aleatorio si no tiene uno asignado
+            Color colorAl = generarColorAleatorio();
+            coloresAeropuertos.put(a,colorAl);
+            double x = lonToX(a.getLongitud(), canvasWidth);
+            double y = latToY(a.getLatitud(), canvasHeight);
+            gc.setFill(colorAl); // Color del aeropuerto
+            gc.fillOval(x - 5, y - 5, 10, 10); // Dibuja un circulo de radio 5
+            gc.setFill(Color.BLACK);
+            gc.fillText(a.getCodigo(), x + 8, y); // Escribe el nombre al lado del aeropuerto
+        }
+        //Recorre los vuelos y dibuja las rutas
+        for(Aeropuerto origen : red.getVertices()){ // Recorre los aeropuertos de la red
+            for(Vuelos vuelo : origen.getAdyacentes()){ // Recorre los vuelos adyacentes de cada aeropuerto
+                Aeropuerto destino = vuelo.getTarget(); // Obtiene el Aeropuerto de destino de ese vuelo
+                // Calculan la posición (X, Y) del aeropuerto de origen en el canvas.
+                double x1 = lonToX(origen.getLongitud(), canvasWidth);
+                double y1 = latToY(origen.getLatitud(), canvasHeight);
+                // Calculan la posición (X, Y) del aeropuerto de destino en el canvas.
+                double x2 = lonToX(destino.getLongitud(), canvasWidth);
+                double y2 = latToY(destino.getLatitud(), canvasHeight);
+                // Dibuja la linea del vuelo 
+                gc.setStroke(coloresAeropuertos.get(origen)); // Color de la línea del vuelo origen
+                gc.setLineWidth(2); // Grosor linea
+                gc.strokeLine(x1, y1, x2, y2); // Dibuja la linea que representa el vuelo
+                // Dibuja la flecha en el extremo de la línea
+                drawArrow(gc, x1, y1, x2, y2);
+                //Calculo el punto medio 
+                double midX = (x1 + x2) / 2;
+                double midY = (y1 + y2) / 2;
+                gc.setFill(Color.BLACK); //Color texto
+                gc.fillText(vuelo.getDistancia() + " km", midX, midY - 5); // Escribe la distancia en el punto medio
+            }
+        }
+    }
 
+    // Método para iniciar la aplicación
+    private void cargarDatosIniciales(Red red){
+        red.getVertices().clear();
+        /*
+         * AEROPUERTOS
+        */
+        red.addAeropuerto(new Aeropuerto("PKX", "Daxing","Pekín","China",39.511944, 116.410556));
+        red.addAeropuerto(new Aeropuerto("UIO","Quito", "Quito", "Ecuador", -0.141, -78.488));
+        red.addAeropuerto(new Aeropuerto("MIA","Miami", "Miami", "EEUU", 25.7617, -80.1918));
+        red.addAeropuerto(new Aeropuerto("MAD","Madrid", "Madrid", "España", 40.4168, -3.7038));
+        red.addAeropuerto(new Aeropuerto("CDG","París", "París", "Francia", 49.0097, 2.5479));
+        red.addAeropuerto(new Aeropuerto("NRT","Tokio", "Tokio", "Japón", 35.7767, 140.3186));
+        red.addAeropuerto(new Aeropuerto("SYD","Sídney", "Sídney", "Australia", -33.8688, 151.2093));
+        red.addAeropuerto(new Aeropuerto("AKL","Auckland", "Auckland", "Nueva Zelanda", -37.0083, 174.7947));
+        red.addAeropuerto(new Aeropuerto("SN", "Santo Domingo", "Santo Domingo", "República Dominicana", 18.4861, -69.9312));
+        red.addAeropuerto(new Aeropuerto("DXB", "Dubai International", "Dubái", "Emiratos Árabes Unidos", 25.2532, 55.3657));
+        /*
+         * VUELOS
+         */
+        red.addVuelo("PKX", "DXB", 220, 200.15,"China Airlines");
+        red.addVuelo("PKX", "CDG", 340, 230.23,"China Airlines");
+        red.addVuelo("UIO", "MIA", 230, 600.30,"LATAM Airlines");
+        red.addVuelo("NRT", "MAD" , 542, 438,"Avianca");
+        red.addVuelo("UIO", "LIM", 70, 123.12,"LATAM Airlines");
+        red.addVuelo("LIM", "MIA", 70, 230.30,"LATAM Airlines");
+        red.addVuelo("MIA", "NRT", 340, 600.29,"American Airlines");
+        red.addVuelo("SYD", "NRT", 320, 502.53,"Avianca");
+        red.addVuelo("SN", "MAD", 130, 134.32,"Avianca");
+
+    }
+    
+    
     public static void main(String[] args) {
         launch(args);
     }
